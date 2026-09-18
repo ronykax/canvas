671972a814cd5dd9bf58948d210080015090e6dc
Rony Kati <ronykax@gmail.com>
2026-09-18 16:49:13 +0530

    clean up

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -1,1 +1,6 @@
-export const App = () => <div />;
+export const App = () => (
+  <div className="h-screen w-full">
+    <div className="h-full w-xs bg-red-400" />
+    <div className="h-full flex-1" />
+  </div>
+);
